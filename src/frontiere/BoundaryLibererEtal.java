5271d1e188d3a1439986d8bc047ace886f59c9fd
YNS317 <145299495+YNS317@users.noreply.github.com>
2024-11-13 02:05:48 +0100

    2.6 finit

--- src/frontiere/BoundaryLibererEtal.java
+++ src/frontiere/BoundaryLibererEtal.java
@@ -10,7 +10,17 @@ public class BoundaryLibererEtal {
 	}
 
 	public void libererEtal(String nomVendeur) {
-		//TODO a completer
+		if (!controlLibererEtal.isVendeur(nomVendeur)) {
+			System.out.println("Mais vous n'êtes pas inscrit sur notre marché aujourd'hui !\n");
+		}else {
+			String[] donneesEtal = controlLibererEtal.libererEtal(nomVendeur);
+			if (Boolean.parseBoolean(donneesEtal[0])) {
+				System.out.println("Vous avez vendu "+donneesEtal[4]+" sur "+donneesEtal[3]+" "+donneesEtal[2]
+						+"\n");
+				System.out.println("En revoir "+nomVendeur+" passez une bonne journee\n");
+			}
+			
+		}
 	}
 
 }
